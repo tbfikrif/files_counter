4cc188039591b28dc684bfe3ef4eed04d281972e
fikri-crowdo <tubagus.fatoni@crowdo.com>
2022-11-09 21:00:28 +0700

    Add description to readme & handle error if no path parameter

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# files_counter
+# README
+
+### Configuration
+
+```
+bundle install
+```
+
+### How To Run
+
+```
+puma
+```
+
+### How To Try Counting Files (Open In Web Browser)
+
+```
+http://localhost:9292/?path=<path files directory>
+```
--- app.rb
+++ app.rb
@@ -1,27 +1,33 @@
+require 'json'
+
 class App
   def call(env)
     headers = { 'Content-Type' => 'text/html' }
 
     return [200, headers, ['favicon']] if env['PATH_INFO'] == '/favicon.ico'
 
-    path = env['QUERY_STRING'].split('=').last
-    [200, headers, [file_with_same_content(path)]]
+    array = env['QUERY_STRING'].split('&')
+    params = {}
+    array.each do |arr|
+      a = arr.split('=')
+      params[a.first] = a.last
+    end
+    [200, headers, [file_with_same_content(params['path'])]]
   end
 
   private
 
   def file_with_same_content(dir)
+    return 'Need <b>path</b> parameter' if dir.nil?
     files = []
     Dir[File.join(dir, '*')].each do |file|
       files << File.read(file)
     end
   
     hash = Hash.new(0)
-    files.each do |v|
-      hash[v] += 1
-    end
+    files.each { |v| hash[v] += 1 }
   
-    max = hash.max_by{|k, v| v}
+    max = hash.max_by{ |k, v| v }
     if max.nil?
       'There is no file or path is wrong.'
     else
